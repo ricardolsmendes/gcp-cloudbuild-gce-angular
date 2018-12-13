f7ae535ee463308d79d32aff8dde502ebc133d3a
Ricardo Mendes <ricardolsmendes@gmail.com>
2018-12-13 01:33:33 -0200

    Improve cloudbuild.yaml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gcp-cloudbuild-gce-angular-new';
+  title = 'gcp-cloudbuild-gce-angular';
 }
